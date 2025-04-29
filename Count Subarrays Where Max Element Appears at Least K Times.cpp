abef045ed65ae92966a7ffde7d70fc08c0987087
Ritesh Kumar <147717204+ritesh031@users.noreply.github.com>
2025-04-29 23:29:06 +0530

    leetcode 2962

--- Count Subarrays Where Max Element Appears at Least K Times.cpp
+++ Count Subarrays Where Max Element Appears at Least K Times.cpp
@@ -4,6 +4,8 @@ Leetcode: 2962
 Problem: Count Subarrays Where Max Element Appears at Least K Times ( return the count of subarray where maxElement of array present at least k times ) 
 */
 
+/**************************************************************************Ritesh**********************************************************************/
+
 // Approach-1 --> Using Sliding Window ( our Khandani Taemplet ) 
 // TC: O(n)
 // SC: O(1)
@@ -38,4 +40,31 @@ public:
 };
 
 
-// Approach-2 
+// Approach-2 ---> Without using Sliding Window
+// TC: O(n)
+// SC: O(n) --> we store the maxE indices in maxIndices 
+
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int k) {
+        int maxE = *max_element(begin(nums), end(nums));
+        int n = nums.size();
+
+        long long result = 0;
+
+        vector<int> maxIndices;
+        for(int i=0; i<n; i++) {
+            if(nums[i] == maxE) {
+                maxIndices.push_back(i);     // store the index of maxE in maxIndices array
+            }
+
+            int size = maxIndices.size();
+
+            if(size >= k) {                     // This is main part to focus
+                int last_i = maxIndices[size-k];
+                result += (last_i + 1);
+            }
+        }
+        return result;
+    }
+};
